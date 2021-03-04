3e80df48e9abf12814d18532354a0b99c0b08c44
= <=>
2021-03-04 17:26:07 -0300

    First commit - finalizando exercicio

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//COMPONENTE EM CLASSE
+//Definimos o state no construtor para alterar usamos o método setState
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      textoAExibir: "Mega Sena",
+      contadores: [0,0,0,0,0,0]
+    }
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.textoAExibir}</Text>
+        <Text>{this.state.contadores.map((contador)=> `${contador} `)}</Text>
+        <Button
+          title="Alterar Texto"
+          onPress={_=>this.setState({textoAExibir: 'Novo Texto'})}
+        />
+        <Button
+          title="Sortear"
+          onPress={_=>{
+            const cont = this.state.contadores.map(() => Math.floor(( 1 + Math.random() * (60 - 1))))
+            console.log(cont)
+            this.setState({contadores: cont})
+
+            }
+          }
+        />
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//COMPONENTE FUNCIONAL
+//Defimos o State como um Hook (useState)
+// // hook react
+// //flex direction padrão column
+// export default function App() {
+//   const [textoAExibir, setTextoAExibir] = useState('Texto Inicial');
+//   return (
+//     <View style={styles.container}>
+//       <Text>{textoAExibir}</Text>
+//       <Button
+//         title="Alterar texto"
+//         onPress={()=> {setTextoAExibir('Novo Texto')}}
+//       />
+//     </View>
+//   );
+// }
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  }
+);
